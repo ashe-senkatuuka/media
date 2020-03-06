e43d3607661a7d9f1266aad381f97c2ee60ee51a
ashe-senkatuuka <ashemukisa@gmail.com>
2020-03-06 13:02:10 +0300

    facebookpage

---
+++ js/behaviour.js
@@ -0,0 +1,34 @@
+//console.log('I am working')
+
+/*
+Data Types
+-Number (integer, float, double)
+-String (character, string)
+-Boolean (true/false, yes/no, 1/0)
+-object (array, date)
+-function
+-undefined
+-null
+*/
+
+// VARIABLES
+
+
+
+let name
+let initial
+name = "AShe"
+initial ="A"
+let y = null;
+let z;
+
+
+let male
+male = true
+console.log(typeof male)
+
+let marks = {math:90,sci:70}
+console.log(marks)
+
+
+
